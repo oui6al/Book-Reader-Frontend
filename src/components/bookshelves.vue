ede426280d52da351d53ce2268d9b94ce374bdc2
WAMANR <adil.rouichi011@gmail.com>
2024-02-29 10:09:30 +0100

    ajout de l'historique et de la suggestion

--- src/components/bookshelves.vue
+++ src/components/bookshelves.vue
@@ -1,11 +1,39 @@
-
 <template>
-  <div class="book-gallery">
-    <h2 :title="title">{{ title }} </h2> 
-    <div class="gallery-container">
-      <div class="gallery" ref="gallery">
-        <div v-for="book in books" :key="book.id" class="book-item">
-          <img :src="book.formats['image/jpeg']" alt="Book Cover" class="book-image" @click="readBook(book.id)"/>
+  <div>
+    <!-- Galerie principale-->
+    <div class="book-gallery">
+      <h2 :title="title">{{ title }} </h2>
+      <div class="gallery-container">
+        <div class="gallery" ref="gallery">
+          <div v-for="book in books" :key="book.id" class="book-item">
+            <img :src="book.formats['image/jpeg']" alt="Book Cover" class="book-image" @click="readBook(book)" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Galerie d'historique -->
+    <div class="book-gallery">
+      <h2>Historique</h2>
+      <div class="gallery-container">
+        <div class="gallery" ref="gallery">
+          <div v-for="historyBook in historyBooks" :key="historyBook.id" class="book-item">
+            <img :src="historyBook.formats['image/jpeg']" alt="Book Cover" class="book-image"
+              @click="readBook(historyBook)" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+     <!-- Galerie de suggestion -->
+     <div class="book-gallery">
+      <h2>Suggestion</h2>
+      <div class="gallery-container">
+        <div class="gallery" ref="gallery">
+          <div v-for="suggestionBook in suggestionBooks" :key="suggestionBook.id" class="book-item">
+            <img :src="suggestionBook.formats['image/jpeg']" alt="Book Cover" class="book-image"
+              @click="readBook(suggestionBook)" />
+          </div>
         </div>
       </div>
     </div>
@@ -16,7 +44,7 @@
 
 export default {
   components: {
-   
+
   },
   props: {
     title: {
@@ -27,18 +55,42 @@ export default {
       type: Array,
       required: true,
     },
+    historyBooks: {
+      type: Array,
+      required: true
+    },
+    suggestionBooks: {
+      type: Array,
+      required: true
+    }
   },
   methods: {
-    readBook(bookId) {
-      this.$router.push({ name: 'reader', params: { id: bookId } });
-      console.log("readBook", bookId);
+    readBook(book) {
+      this.$router.push({ name: 'reader', params: { id: book.id } });
+      console.log("readBook", book.id);
+
+      // Store history
+      let storedBooks = JSON.parse(localStorage.getItem('historyBooks')) || [];
+      const bookId = book.id;
+
+      const index = storedBooks.findIndex((storedBook) => storedBook.id === bookId);
+      if (index !== -1) {
+        storedBooks.splice(index, 1);
+      }
+
+      storedBooks.unshift(book);
+      console.log("History", storedBooks);
+
+      localStorage.setItem('historyBooks', JSON.stringify(storedBooks));
     },
-}
-    
+  }
 };
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+}
 
 .book-gallery {
   position: relative;
@@ -49,7 +101,7 @@ export default {
 
 .gallery-container {
   width: 100%;
-  overflow-x:hidden;
+  overflow-x: hidden;
   position: relative;
   justify-content: center;
   margin-top: 3rem;
@@ -77,6 +129,4 @@ export default {
   box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.2);
   cursor: pointer;
 }
-
-
 </style>
